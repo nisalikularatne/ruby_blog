a7098ebc53e34d10d20953a51e68c000f6117c1f
nisalikularatne <nisalikularatne@yahoo.com>
2020-01-08 17:34:01 +0800

    Tagging feature completed

--- db/migrate/20200107085422_create_comments.rb
+++ db/migrate/20200107085422_create_comments.rb
@@ -3,7 +3,7 @@ class CreateComments < ActiveRecord::Migration[6.0]
     create_table :comments do |t|
       t.string :author_name
       t.text :body
-      t.references :article, null: false, foreign_key: true
+      t.references :article, null: false, foreign_key: true,on_delete: :cascade
 
       t.timestamps
     end
---
+++ db/migrate/20200108031317_create_taggings.rb
@@ -0,0 +1,9 @@
+class CreateTaggings < ActiveRecord::Migration[6.0]
+  def change
+    create_table :taggings do |t|
+      t.references :tag, null: false, foreign_key: true, on_delete: :cascade
+      t.references :article, null: false, foreign_key: true, on_delete: :cascade
+      t.timestamps
+    end
+  end
+end
